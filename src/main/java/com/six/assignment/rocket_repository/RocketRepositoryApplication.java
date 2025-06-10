1aa4fcd47bd6b4e241306f5adf0c1d8b5ef2c4a6
Bartek Kaczorowski <bart.kaczorowski@gmail.com>
2025-06-10 10:24:11 +0200

    project generation from https://start.spring.io/ ; java 21 LTS, no deps

---
+++ src/main/java/com/six/assignment/rocket_repository/RocketRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.six.assignment.rocket_repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/six/assignment/rocket_repository/RocketRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.six.assignment.rocket_repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
